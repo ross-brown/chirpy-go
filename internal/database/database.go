2f4c443d554a1eddf5233bc59301318add0286ff
ross-brown <rbrown_98@yahoo.com>
2024-04-30 14:00:44 -0500

    add users to db and debug flag

--- internal/database/database.go
+++ internal/database/database.go
@@ -14,11 +14,17 @@ type DB struct {
 
 type DBStructure struct {
 	Chirps map[int]Chirp `json:"chirps"`
+	Users  map[int]User  `json:"users"`
 }
 
 type Chirp struct {
 	ID   int    `json:"id"`
 	Body string `json:"body"`
+}
+
+type User struct {
+	ID    int    `json:"id"`
+	Email string `json:"email"`
 }
 
 // Creates a new DB connection and creates the DB file if not exist
@@ -82,9 +88,31 @@ func (db *DB) GetChirp(id int) (Chirp, error) {
 	return chirp, nil
 }
 
+func (db *DB) CreateUser(email string) (User, error) {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return User{}, err
+	}
+
+	id := len(dbStructure.Users) + 1
+	user := User{
+		ID:    id,
+		Email: email,
+	}
+	dbStructure.Users[id] = user
+
+	err = db.writeDB(dbStructure)
+	if err != nil {
+		return User{}, err
+	}
+
+	return user, nil
+}
+
 func (db *DB) createDB() error {
 	dbStructure := DBStructure{
 		Chirps: map[int]Chirp{},
+		Users:  map[int]User{},
 	}
 	return db.writeDB(dbStructure)
 }
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/ross-brown/chirpy-go/internal/database"
 )
@@ -14,6 +16,17 @@ type apiConfig struct {
 }
 
 func main() {
+	debugPtr := flag.Bool("debug", false, "Enable debug mode")
+	flag.Parse()
+
+	if *debugPtr {
+		err := resetDatabase()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("database.json wiped")
+	}
+
 	const PORT = "8080"
 	const FILEPATH_ROOT = "."
 
@@ -36,6 +49,7 @@ func main() {
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerCreateChirp)
 	mux.HandleFunc("GET /api/chirps", apiCfg.handlerGetChirps)
 	mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerGetChirp)
+	mux.HandleFunc("POST /api/users", apiCfg.handlerCreateUser)
 
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 
@@ -48,4 +62,21 @@ func main() {
 
 	fmt.Printf("Server listening on port %s...\n", PORT)
 	server.ListenAndServe()
+}
+
+func resetDatabase() error {
+	_, err := os.Stat("database.json")
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return err
+	}
+
+	err = os.Remove("database.json")
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
